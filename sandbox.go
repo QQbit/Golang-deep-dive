0b76533c69fb6c3108d41a1b7870394d99ef1fcd
kim <thanawut.pad@gmail.com>
2022-10-22 22:28:36 +0700

    Short declaration

--- sandbox.go
+++ sandbox.go
@@ -2,40 +2,26 @@ package main
 
 import "fmt"
 
+test := 123  // อีก limit หนึ่งของ short declaration 
+//syntax error: non-declaration statement outside function body เพราะ line 5 ไม่ใช่ declaration statement แต่มันเป็น short declaration 
+var test2 = 1 //ต้องใช้แบบนี้ถึงได้
+
 func main() {
-	// Go variable declearation
-	// 1. var name type = expression
-	var x int = 4
-	fmt.Println(x)
+	// Go short variable declaration
+	// 1. var name = expression
+	var a = 4
+	fmt.Println(a)
+	b := 4
+	fmt.Println(b)
+	x, y := 1, 2
+	fmt.Println(x, y)
+	//x, y := y, x no new variables on left side of :=
+	//ทำไม่ได้ กฏของ go คือต้องมีตัวใดตัวหนึ่งเป็น varible ใหม่
+	x, y1 := y, x
+	fmt.Println(x, y1)
 
-	// 2. var name = expression แบบ omit type
-	var y = 5 //มันก็จะทำการ deduce type
-	fmt.Println(y)
-
-	var a = 5
-	var b = 5.0
-	var c = "string"
-	fmt.Printf("%T\n", a) //int
-	fmt.Printf("%T\n", b) //float64
-	fmt.Printf("%T\n", c) //string
-
-	// 3. var name type แบบ omit expression
-	var d int
-	var e string
-	fmt.Println(d) // zero value of int is 0
-	fmt.Println(e) // zero value of string is "" (empty string)
-
-	//ถ้าไม่ใช่ primitive type เป็นแบบ reference type
-	var f []string        //ยกตัวอย่างเป็น slices of string
-	fmt.Println(f == nil) //true  zero value of slices is nil
-	fmt.Println(f)        // []
-
-	//Go สามารถประกาศตัวแปรหลายๆตัวได้ ใน statement เดียวกัน
-	var g, h, i int
-	fmt.Printf("%T %T %T\n", g, h, i)
-	fmt.Printf("%v %v %v\n", g, h, i)
-
-	var j, l, m = 123, "hello", 4.5
-	fmt.Printf("%T %T %T\n", j, l, m)
-	fmt.Printf("%v %v %v\n", j, l, m)
+	//มาดูกันว่าทำไม เราถึง short declaration 2 รอบไม่ได้
+	i := 1 //var x = 1 
+	i := 2 //var x = 2  - no new variables on left side of :=  ก็คือ variable เราประกาศไว้แล้ว เราประกาศอีกมันก็ผิดกฏ 
+	fmt.Println(i)
 }
