fcf5bb9c39c08b90fa22b61e2cdacdf64ea40058
kim <thanawut.pad@gmail.com>
2022-10-23 23:34:16 +0700

    Pointer concept

--- sandbox.go
+++ sandbox.go
@@ -2,26 +2,25 @@ package main
 
 import "fmt"
 
-test := 123  // อีก limit หนึ่งของ short declaration 
-//syntax error: non-declaration statement outside function body เพราะ line 5 ไม่ใช่ declaration statement แต่มันเป็น short declaration 
-var test2 = 1 //ต้องใช้แบบนี้ถึงได้
-
+// address จะเปลี่ยนทุกรอบที่รัน เพราะขึ้นอยู่กับ os จะจัดการว่าให้โปรแกรมของเราไปรันใน section ไหนของ memory
 func main() {
-	// Go short variable declaration
-	// 1. var name = expression
-	var a = 4
-	fmt.Println(a)
-	b := 4
-	fmt.Println(b)
-	x, y := 1, 2
-	fmt.Println(x, y)
-	//x, y := y, x no new variables on left side of :=
-	//ทำไม่ได้ กฏของ go คือต้องมีตัวใดตัวหนึ่งเป็น varible ใหม่
-	x, y1 := y, x
-	fmt.Println(x, y1)
+	x := 70
+	var p *int
+	fmt.Println("&x", &x) //represent address in memory
+	fmt.Println("&p", &p) //เป็น nil เพราะไม่มีค่า แต่จะต้องมี address ที่ associate มันใน main memory
+	p = &x
+	fmt.Println("p", p)           //0x14000126008
+	fmt.Println("vale of *p", *p) //dereference pointer ก็จะได้ value ของ x ซึ้งก็คือ 70
+	fmt.Println("*(&x)", *&x)     //70
+	/**
+	-------------------------------------------------
+	| variable | address   		|   value   		|
+	-------------------------------------------------
+	| 	 x	   | 0x14000126008	| 	 70				|
+	-------------------------------------------------
+	| 	 p	   | 0x14000120018	| 	 0x14000126008	|
+	-------------------------------------------------
 
-	//มาดูกันว่าทำไม เราถึง short declaration 2 รอบไม่ได้
-	i := 1 //var x = 1 
-	i := 2 //var x = 2  - no new variables on left side of :=  ก็คือ variable เราประกาศไว้แล้ว เราประกาศอีกมันก็ผิดกฏ 
-	fmt.Println(i)
+
+	**/
 }
