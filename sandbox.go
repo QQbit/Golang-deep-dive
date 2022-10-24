0a8d6947b31bf26760c8c138386c16c38418c5b4
kim <thanawut.pad@gmail.com>
2022-10-25 00:03:42 +0700

    Control flow if and else

--- sandbox.go
+++ sandbox.go
@@ -4,20 +4,18 @@ import (
 	"fmt"
 )
 
-type KG float64
-type LB float64
-
-func (lb LB) toKg() KG {
-	return KG(lb / 2.2046226218)
-}
-
-func (kg KG) toString() string {
-	return fmt.Sprintf("%v kg", kg)
-}
-
 func main() {
-	k := KG(3)
-	b := LB(3)
-	fmt.Println(k > b.toKg())
-	fmt.Println(k.toString())
+	x := 5
+	if (x <= 5) || false {
+		fmt.Println("if condition")
+	} else {
+		fmt.Println("else condition")
+	}
+
+	// อีก feature หนึ่งของ go เราสามารถประกาศ local variable สำหรับ block ของ if - else ได้
+	if v := x + 1; (x <= 5) || false {
+		fmt.Println("if condition", v)
+	} else {
+		fmt.Println("else condition", v)
+	}
 }
