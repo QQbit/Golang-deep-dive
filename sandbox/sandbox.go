2fef91d713c9954185c2bcb39599c2b4d469c572
kim <thanawut.pad@gmail.com>
2022-10-31 01:10:44 +0700

    Basic-type integer

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -2,19 +2,11 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"prime"
+	"math"
 )
 
 func main() {
-
-	for i := 0; i < 100; i++ {
-		x := rand.Intn(1000000)
-		fmt.Printf("%d , %t\n", x, prime.IsPrime(x))
-	}
-
-	fmt.Println(prime.IsPrime(2))
-	fmt.Println(prime.IsPrime(3))
-	fmt.Println(prime.IsPrime(4))
-	fmt.Println(prime.IsPrime(491023)) //ถ้าเกิดว่าทุกครั้งที่เราเรียก isPrime แล้วมาต้องคำนวณใหม่หมด 1,000,000 ครั้ง เราจะต้องมาเช็คว่าตรงนั่นเป็น prime หรือเปล่า มันช้า
+	fmt.Println(math.MinInt8, math.MaxInt8)
+	fmt.Println(math.MaxUint8)
+	fmt.Println(math.MinInt16, math.MaxInt16)
 }
